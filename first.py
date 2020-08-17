bcc6cbef7323d2cda3d01904cce1eea46e6e38ea
FethiOuail <anwarinfo26@gmail.com>
2020-08-17 15:14:20 +0100

    test1

---
+++ first.py
@@ -0,0 +1,20 @@
+# ---------------------------------
+# Information About File
+# Licence
+# Who Created The File
+#
+# ---------------------------------
+print("I love Python ");
+print("I love Kotlin ")
+
+# This is commants
+
+# ------------------------
+# type()
+# All Data in Python is Object
+# -------------------------
+
+print(type(55))
+
+help("keywords")
+
---
+++ src/test1.py
@@ -0,0 +1,19 @@
+from builtins import print
+
+myStringOne = "anwar kamel"
+myStringTwo = "ouail 'A' "
+
+print(myStringOne)
+print(myStringTwo)
+
+myStringFive = "First \
+Second \
+Thrid"
+
+print(myStringFive)
+
+myText = """My name is anwar
+kamel Ouail I live in Aljgeria"""
+
+print(myText)
+
